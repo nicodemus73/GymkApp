2f87e2ce8a78dcccc94af219a2982494b103cc32
laura.mayals <laura.mayals@est.fib.upc.edu>
2020-03-20 17:46:11 +0100

    servidor + bbdd

---
+++ src/server/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+router.get('/hola', (req, res) => {
+    res.send("hello");
+});
+
+router.get('/adeu', (req, res) => {
+    res.send("adeu");
+});
+
+/*router.get('/add_user', async(req, res) => {
+    console.log(new Task(req.body)); //crea el objeto a almacenar
+    const task = new Task(req.body);
+    await task.save();
+    res.send(new Task(req.body));
+});*/
+
+module.exports = router;
