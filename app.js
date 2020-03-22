c3eaf572e317f0bb204b8c59c798f1e80171aba0
Penafiel i Ramia <g.penafiel.i.ramia@accenture.com>
2020-03-22 17:43:17 +0100

    actualització estructura de directoris I

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
+const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// Middlewares. They execute when accessing the route.
-/*
-app.use('/llocs', () => {
-    console.log('This is a middleware running');
-    // Es pot usar per autentificar
+// connecting to db
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log('Db connected'))//promesa
+    .catch(err => console.log(err));
+
+//importando rutas
+const indexRoutes = require('./server/routes/index');
+
+//settings
+//app.set('port', process.env.PORT || 3000);
+
+app.use(express.urlencoded({extended: false})) //entender json, el false es porque
+//solo sera texto y no ocupa mucho --> cambiar
+
+//routes
+app.use('/', indexRoutes);
+
+// stating the server
+//app.listen(app.get('port'), () => {
+//console.log(`Server on port ${app.get('port')}`);
+//});
+app.listen(3001, () => {
+    console.log('server port 3001');
 });
-*/
-
-//Import routes
-const postsRoute = require('./routes/posts');
-const usersRoute = require('./routes/users');
-
-app.use('/posts', postsRoute);
-app.use('/users', usersRoute);
-
-// Start listening
-app.listen(3000);
-
-
