5ea8d5b293c467e255d7e094a1e6818b87311ace
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-03-24 20:09:14 +0100

    test BotonAMapa, mapa no aparece

--- app/GymkApp/app/src/main/java/gymkapp/main/MainActivity.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/MainActivity.kt
@@ -1,12 +1,19 @@
 package gymkapp.main
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  fun onClickButtonToMap(view: View){
+    startActivity(Intent(this,MapsActivity::class.java))
+  }
 }
