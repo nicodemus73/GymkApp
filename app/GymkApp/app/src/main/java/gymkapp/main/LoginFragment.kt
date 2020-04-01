41efb38ead32a1092184321a2bfb47687f5956d6
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-04-01 21:23:08 +0200

    SharedPreferences

--- app/GymkApp/app/src/main/java/gymkapp/main/LoginFragment.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/LoginFragment.kt
@@ -5,25 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.login.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class LoginFragment : Fragment() {
 
   /**
    * Instancia que sirve para mantener datos independientemente del estado del fragmento. ViewModel
    * provee funciones para gestionar el estado del login, el estado del registro, para cambiar estados
    * y guardar datos sobre el usuario a lo largo de la aplicacion como el nombre de usuario o el authenticationToken
-   * Esta instanciación solo mantiene los datos en el "scope" del fragmento.
-   * Para mantener la instancia durante la actividad (en una arquitectura de una sola actividad esto
-   * seria muy util) mejor utilizar activityViewModel()
    */
-  val model: LoginViewModel by viewModels()
+  private val viewModel: LoginViewModel by activityViewModels()
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? = inflater.inflate(R.layout.login,container,false)
 
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    val navController = findNavController()
+    buttonBack2.setOnClickListener { navController.navigateUp() }
+  }
 }
--- app/GymkApp/app/src/main/java/gymkapp/main/LoginViewModel.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/LoginViewModel.kt
@@ -1,8 +1,13 @@
 package gymkapp.main
 
+import android.content.SharedPreferences
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Exception
 
+/**
+ * Clase para gestionar el estado de sesión del usuario
+ */
 class LoginViewModel: ViewModel() {
 
   enum class AuthenticationState {
@@ -11,10 +16,29 @@ class LoginViewModel: ViewModel() {
     INVALID_AUTHENTICATION
   }
 
+  //Propiedad observable que representa el estado actual de la sesion
   val authenticationState = MutableLiveData<AuthenticationState>()
+  //valor en memoria del loginToken
+  private var loginToken = ""
 
   init {
-    authenticationState.value = AuthenticationState.UNAUTHENTICATED //Comprobar si el existe un "LoginToken" mediante el sharedpreferences
+    authenticationState.value = AuthenticationState.AUTHENTICATED
   }
+
+  /**
+   * Comprueba que el usuario activo tenga un login token, si no lo tiene se le asigna uno
+   * Si el usuario no tiene un loginToken valido (existe en el modelo o en almacenamiento local) entonces
+   * se cambia el estado a "UNAUTHENTICATED"
+   * Al entrar en esta funcion el valor por defecto del authenticationState deberia ser "AUTHENTICATED"
+   */
+  fun check(pref: SharedPreferences){
+
+    if(loginToken.isEmpty() && try {
+        pref.getString(R.string.TokenKey.toString(),"")!!.also { loginToken=it }.isEmpty()
+      } catch(e: Exception){ true }){
+      authenticationState.value = AuthenticationState.UNAUTHENTICATED
+    }
+  }
+
 
 }
