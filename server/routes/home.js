8cc109818852115dd33aead3d050cf64c3f28979
Guillem Peñafiel <pengu04@gmail.com>
2020-04-06 19:22:36 +0200

    DockerHub test 1

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("This is home");
+    res.send("This is home. Docker Hub test 1");
 });
 
 module.exports = router;
