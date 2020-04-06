1264d61d925d5fa31c9416f72cb01e050ef02afa
Guillem Peñafiel <pengu04@gmail.com>
2020-04-06 20:03:21 +0200

    DocherHub test 2: la venganza

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send("This is home. Docker Hub test 1");
+    res.send("This is home. Docker Hub test 2");
 });
 
 module.exports = router;
