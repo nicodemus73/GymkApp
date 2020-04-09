f8b0fddd0bde8dbd7d301cdf6773d7451b309a08
Guillem Peñafiel <pengu04@gmail.com>
2020-04-09 21:54:26 +0200

    Test autodeploy 1

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).send("This is home. Response codes I");
+    res.status(200).send("This is home. Test autodeploy 1");
 });
 
 module.exports = router;
