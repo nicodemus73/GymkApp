c4571155ee9c1154080d299caea657d589b5e172
Guillem Peñafiel <pengu04@gmail.com>
2020-04-10 17:55:21 +0200

    users

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const Usuario = require('../bbdd/UserSchema');
-const {registerValidation, loginValidation} = require('../bbdd/Validation');
+const User = require('../bbdd/UserSchema');
+const { registerValidation, loginValidation } = require('../bbdd/Validation');
 
 router.get('/', async (req, res) => {
     try {
-        await Usuario.find({}, function (err, users) {
-            if (err) return res.status(500).json({"error": err.message});//Internal Server Error
+        await User.find({}, function (err, users) {
+            if (err) return res.status(500).json({ "error": err.message });//Internal Server Error
             res.status(200).json(users);
         })
     } catch (err) {
-        res.status(500).json({"error": err.message});//Internal Server Error
+        res.status(500).json({ "error": err.message });//Internal Server Error
     }
 });
 
@@ -22,14 +22,12 @@ router.post('/register', async (req, res) => {
 
     try {
         //validar antes de crear el usuario
-
-        const {err} = registerValidation(req.body);
-        if (err) return res.status(400).json({"error": err.details[0].message});
+        const { err } = registerValidation(req.body);
+        if (err) return res.status(400).json({ "error": err.details[0].message });
 
         //validar que username sigui unic
-
-        const usernameExist = await Usuario.findOne({username: req.body.username});
-        if (usernameExist) return res.status(409).json({"error":"Username already exists"}); //conflict
+        const usernameExist = await User.findOne({ username: req.body.username });
+        if (usernameExist) return res.status(409).json({ "error": "Username already exists" }); //conflict
 
         //hashear la password 
         const salt = await bcrypt.genSalt();
@@ -42,57 +40,52 @@ router.post('/register', async (req, res) => {
             //firstname: req.body.firstname,
             //lastname: req.body.lastname,
         });
-            const savedUser = await usern.save(10);
-            res.status(200).end();//.json({userid: usern._id}); //ok
-    } catch(err) {
+        const savedUser = await usern.save(10);
+        res.status(200).end();//.json({userid: usern._id}); //ok
+    } catch (err) {
         console.error(err);
-        res.status(400).json({"error": err.message}); //bad request
+        res.status(400).json({ "error": err.message }); //bad request
     }
 });
 
 
-router.post('/login', async(req, res) => {
-try {
+router.post('/login', async (req, res) => {
+    try {
 
-    //validacion antes del login
-    const {err} = loginValidation(req.body);
-    if (err) return res.status(400).json({"error": err.details[0].message}); //Bad request
-
-    //el usuario ha de existir
-    const username = await Usuario.findOne({username: req.body.username});
-    if (!username) return res.status(404).json({"error": "Incorrect Username or Password"}); //Not Found  no existe el usuario
+        //validacion antes del login
+        const { err } = loginValidation(req.body);
+        if (err) return res.status(400).json({ "error": err.details[0].message }); //Bad request
 
         //el usuario ha de existir
-        const username = await Usuario.findOne({ username: req.body.username });
-        if (!username) return res.json({ error: "Username doesn't exist" });
+        const username = await User.findOne({ username: req.body.username });
+        if (!username) return res.status(404).json({ "error": "Incorrect Username or Password1" }); //Not Found  no existe el usuario
 
-    const validPasswd = await bcrypt.compare(req.body.password, username.password);
-    if(!validPasswd) return res.status(404).json({"error": "Incorrect Username or Password"}); //not found
+        const validPasswd = await bcrypt.compare(req.body.password, username.password);
+        if (!validPasswd) return res.status(404).json({ "error": "Incorrect Username or Password" }); //not found
 
-    //crear y asignar token al usuario
-    const token = jwt.sign({_id: username._id}, process.env.TOKEN_KEY);
-    res.status(200).header('Authorization', token).end();//.json({"token": token});
-    //res.send('Logged in!');
-} catch(err) {
-    res.status(400).json({ "error": err.message });
-}
+        //crear y asignar token al usuario
+        const token = jwt.sign({ _id: username._id }, process.env.TOKEN_KEY);
+        res.status(200).header('Authorization', token).end();//.json({"token": token});
+        //res.send('Logged in!');
+    } catch (err) {
+        res.status(400).json({ "error": err.message });
+    }
 
 });
-
 
 //delete by _id (usuaris de moment) No s'hauria de borrar, donar de baixa
 router.post('/delete/:id', async function (req, res) {
 
-    Usuario.findByIdAndDelete(req.params.id)
-    .exec()
-    .then(doc => {
-        //console.log(doc);
-        if (!doc) {return res.status(404).json({"error":"Document not found"}).end();}
-        return res.status(200).json({"message":"File deleted"}).end();
-    })
-    .catch (error =>
-        res.status(400).json({message: error}));
+    User.findByIdAndDelete(req.params.id)
+        .exec()
+        .then(doc => {
+            //console.log(doc);
+            if (!doc) { return res.status(404).json({ "error": "Document not found" }).end(); }
+            return res.status(200).json({ "message": "File deleted" }).end();
+        })
+        .catch(error =>
+            res.status(400).json({ message: error }));
 
- });
+});
 
 module.exports = router;
