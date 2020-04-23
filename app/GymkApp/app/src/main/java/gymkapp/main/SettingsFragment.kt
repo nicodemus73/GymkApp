ac1733466a78ff392e7f086a5673d9d56a6ba2c7
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-04-24 00:08:50 +0200

    WIP: limpiando, jerarquia de codigo

--- app/GymkApp/app/src/main/java/gymkapp/main/SettingsFragment.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/SettingsFragment.kt
@@ -2,56 +2,62 @@ package gymkapp.main
 
 import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.content.edit
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import gymkapp.main.databinding.SettingsBinding
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 
-
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
   private val loginModel: LoginViewModel by activityViewModels()
-  private var _bind: SettingsBinding? = null
-  private val bind: SettingsBinding inline get() = _bind!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-
-    _bind = SettingsBinding.inflate(inflater, container, false)
-    return bind.root
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
+  override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 
     val navController = findNavController()
-    bind.nightModeSwitch.setOnCheckedChangeListener { _, isChecked ->
-      if (isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-      else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    val context = preferenceManager.context
+    val screen = preferenceManager.createPreferenceScreen(context)
+
+    val category = PreferenceCategory(context).apply {
+      title = "Settings"
+    }//TODO añadir directamente
+    val nightModePref = SwitchPreferenceCompat(context).apply {
+      key = "nightModeSetting"
+      title = "Night Mode"
+    }
+    val changePasswordPref = Preference(context).apply {
+      key = "changePasswordSetting"
+      title = "Change Password"
     }
 
-    bind.logoutButton.setOnClickListener {
+    val logOutPref = Preference(context).apply {
+      key = "logOutSetting"
+      title = "Log Out"
+      isVisible = true //TODO probar
+    }
 
+    /*
+    preferenceLogOut?.setOnPreferenceClickListener {
       activity?.getPreferences(Context.MODE_PRIVATE)?.edit {
         remove(R.string.TokenKey.toString())
       }
       navController.navigate(SettingsFragmentDirections.toLoginFTUE())
       loginModel.logout()
+      true
     }
-    bind.newSettings.setOnClickListener {
-      navController.navigate(SettingsFragmentDirections.toNewSettings())
-    }
-  }
 
-  override fun onDestroyView() {
-    super.onDestroyView()
-    _bind = null
+    preferenceNightMode?.setOnPreferenceClickListener {
+
+      true
+    }*/
+
+    with(screen){
+      addPreference(category)
+      addPreference(nightModePref)
+      addPreference(changePasswordPref)
+      addPreference(logOutPref)
+    }
   }
 }
