08e0b71a961e0318a7145df51d05ad32936dd68f
laura <lmglmg1999@hotmail.com>
2020-04-30 01:53:03 +0200

    Crides app-server per la demo

--- app/GymkApp/app/src/main/java/gymkapp/main/api/Test.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/api/Test.kt
@@ -5,9 +5,9 @@ import com.google.maps.android.data.geojson.GeoJsonLayer
 import com.google.maps.android.data.geojson.GeoJsonPoint
 import gymkapp.main.api.RemoteAPI
 
-fun main() {
+suspend fun main() {
 
-  val layer = GeoJsonLayer(null,null)
+  /*val layer = GeoJsonLayer(null,null)
   val point = GeoJsonPoint(LatLng(1.0,2.0))
   val properties = hashMapOf("Ocean" to "South Atlantic")
   val pointFeature = GeoJsonFeature(point,"Origin",properties,null)
@@ -23,8 +23,10 @@ fun main() {
     isDraggable = true
     title = "Hello"
     snippet = "I am a draggable marker"
-  }
+  }*/
   //PRIMERO!!
   //RemoteAPI.initMapsCallsClient("aefnjoafheanso")
   //RemoteAPI.listNearMaps(...)
+//  RemoteAPI.initMapsCallsClient("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTkwNTA3NTQ2ZWIyYzA1YTQxZTRlNWYiLCJpYXQiOjE1ODY1MTYxMDN9.Tx2kqrfQMLGQGClpCQxujoe6zWnnxy7TSe219kzRBsQ")
+//  RemoteAPI.obtainStartMap()
 }
