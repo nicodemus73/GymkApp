36a8cd74c98799668535cfa03304705684fc65d1
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-05-01 05:01:23 +0200

    reviewSettings funcional. Pendiente de integrar

--- app/GymkApp/app/src/main/java/gymkapp/main/ui/settings/SettingsFragment.kt
+++ app/GymkApp/app/src/main/java/gymkapp/main/ui/settings/SettingsFragment.kt
@@ -2,17 +2,20 @@ package gymkapp.main.ui.settings
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.core.content.edit
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.preference.*
+import gymkapp.main.R
 import gymkapp.main.viewmodel.LoginViewModel
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
   private val loginModel: LoginViewModel by activityViewModels()
 
-  fun nightMode(enable: Boolean?) {
+  private fun nightMode(enable: Boolean?) {
     enable?.let { setDefaultNightMode(if (it) MODE_NIGHT_YES else MODE_NIGHT_NO) }
       ?: setDefaultNightMode(
         MODE_NIGHT_FOLLOW_SYSTEM
@@ -22,8 +25,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
   override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
 
     val classTag = javaClass.simpleName
+    Log.d(classTag,"Preerencias creadas")
 
-    //val navController = findNavController()
     val context = preferenceManager.context
     val screen = preferenceManager.createPreferenceScreen(context)
 
@@ -69,7 +72,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         requireActivity().getPreferences(Context.MODE_PRIVATE)?.edit {
           remove(R.string.TokenKey.toString())
         }
-        //navController.navigate(SettingsFragmentDirections.toLoginFTUE())
+        findNavController().navigate(SettingsFragmentDirections.toLoginFTUE())
         loginModel.logout()
         true
       }
